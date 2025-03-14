422d49579a3755c557bf9cc4c0880bb5ba11f349
Tomas Toledo <trtoledo1977@gmail.com>
2025-03-14 17:14:17 -0300

    Select a featured puppy

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,27 @@ import {puppyList} from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
-  //const [featuredPup, setFeatPupId] = useState(null);
-  console.log(puppyList)
+  const [featPupId, setFeatPupId] = useState(null)
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  
+  console.log(featuredPup)
 
   return (
     <div className="App">
+      { featPupId && <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div> } 
       {
         puppies.map((puppy) => {
-             return <p key={puppy.id}>{puppy.name}</p>;
-           })
-       }
+             return (
+             <p onClick={()=> { setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
+             
+        );
+           })}
     </div>
   );
 }

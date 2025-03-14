271b5af1e74a18ef3ed892967739cc813346ce9d
Tomas Toledo <trtoledo1977@gmail.com>
2025-03-14 16:00:32 -0300

    data with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
-//import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
 import './App.css'
+import {puppyList} from './data.js'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList);
+  //const [featuredPup, setFeatPupId] = useState(null);
+  console.log(puppyList)
 
   return (
-    <>
-    
-    </>
-  )
+    <div className="App">
+      {
+        puppies.map((puppy) => {
+             return <p key={puppy.id}>{puppy.name}</p>;
+           })
+       }
+    </div>
+  );
 }
 
 export default App
